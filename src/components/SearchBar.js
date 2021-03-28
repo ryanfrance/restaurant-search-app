d1454a58674a79e12855300c9a2f2f7ff3b0921e
Ryan France <ryan@parall.ax>
2021-03-28 00:04:05 +0000

    Hook up a search bar component to the search screen, use state to track term

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
   return (
-    <View>
-      <Text>Search Screen</Text>
+    <View style={styles.searchBackground}>
+      <Feather name="search" size={30} style={styles.iconStyle} />
+      <TextInput
+        placeholder="Search"
+        style={styles.inputStyle}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={term}
+        onChangeText={newTerm => onTermChange(newTerm)}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  searchBackground: {
+    backgroundColor: '#F0EEEE',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row'
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15
+  }
+});
 
 export default SearchBar;
